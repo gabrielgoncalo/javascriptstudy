b09907c666c771fd3727fdda35a02d61b4fdea2c
gabrielgoncalo <gabriel.tchutcha@hotmail.com.br>
2023-09-15 22:59:54 -0300

    aula 12 do curso de javascript

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 const http = require('http');
-//const url = require('url');
+const url = require('url');
 
-//const routes = require('./routes');
-const UsersController = require('./controllers/UserController');
-
+const routes = require('./routes');
 
 const server = http.createServer((request, response) =>{
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
+    const parsedUrl = url.parse(request.url, true);
+
+    console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
     
+    const route = routes.find((routeObj) =>(
+        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    
+    ));
 
-    if(request.url === '/users' && request.method ==='GET'){
-        UsersController.listUsers(request, response);
-    } else {
+    if(route){
+        request.query = parsedUrl.query;
+        route.handler(request, response);
+
+    }else {
         response.writeHead(404, {'content-type': 'text/html' });
-        response.end(`Cannot ${request.method} ${request.url}`);
-
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
     }
+   
 
 }); 
 
